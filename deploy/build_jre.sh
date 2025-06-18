43d9703d334cb71604b74ef08d1037b35d74e4bc
Romke van Dijk <github.com@ocbios.nl>
2025-06-18 13:04:12 +0200

    Updating build_jre script

--- deploy/build_jre.sh
+++ deploy/build_jre.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+if [ ! -f ../target/dj-pcfg-0.5.1.jar ];
+then
+	echo "Please build the project before running this script"
+	echo "Run: mvn clean package -DskipTests"
+	exit 1
+fi
 
+version=$(echo -n dj-pcfg-0.5.1.jar | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
+mkdir pcfg_preprocessor-$version
+mkdir cache_server-$version
 wget https://download.oracle.com/graalvm/23/latest/graalvm-jdk-23_linux-x64_bin.tar.gz
 mkdir jdk-23
 tar -xzf graalvm-jdk-23_linux-x64_bin.tar.gz -C jdk-23 --strip-components=1
 jdk-23/bin/jlink --output jre-23 --compress zip-9 --no-header-files --no-man-pages --module-path ../jmods --add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.net.http,java.prefs,java.rmi,java.scripting,java.se,java.security.jgss,java.security.sasl,java.smartcardio,java.sql,java.sql.rowset,java.transaction.xa,java.xml,java.xml.crypto,jdk.graal.compiler
-mv jre-23 pcfg_preprocessor-*
 chmod -R +w jdk-23
-rm -r graalvm-jdk-23_linux-x64_bin.tar.gz jdk-23
+cp -r jre-23 pcfg_preprocessor-$version
+cp -r jre-23 cache_server-$version
+rm -r graalvm-jdk-23_linux-x64_bin.tar.gz jdk-23 jre-23
